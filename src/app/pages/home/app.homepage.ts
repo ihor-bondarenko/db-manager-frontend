d50bbf9200a59022f5d5e576cd44794246c67cfd
Ihor Bondarenko <ihor.bondarenko@acropolium.com>
2017-05-10 18:54:59 +0300

    first commit

---
+++ src/app/pages/home/app.homepage.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.homepage.html',
+    styleUrls: ['./app.homepage.css']
+})
+export class AppHomePage {
+
+}
